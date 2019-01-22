8b2d49aa25b1967cd4ae4cdaf08fa3033f492a6f
Jakob Rath <git@jakobrath.eu>
2019-01-22 10:33:02 +0100

    Allow different return types for variable evaluators

--- src/Mk/Evaluators.hs
+++ src/Mk/Evaluators.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 
 module Mk.Evaluators
   ( allEvaluators
@@ -12,6 +16,7 @@ import Control.Monad.IO.Class
 -- bytestring
 import Data.ByteString (ByteString)
 -- import qualified Data.ByteString as ByteString
+import qualified Data.ByteString.Char8 as B8
 
 -- containers
 import Data.Map (Map)
@@ -28,24 +33,7 @@ import Path
 import Mk.Template
 
 
-
-allEvaluators
-  :: (MonadError String m, MonadIO m)
-  => Path Abs File  -- ^ path to target file
-  -> Map Var (m ByteString)
-allEvaluators target = Map.fromList assocsWithCtx
-  where
-    assocs =
-      [ (Var "HASKELLRESOLVER", testEvaluator)
-      , (Var "HASKELLMODULE", testEvaluator)
-      ]
-    ctx v = Ctx{ ctxVar = v
-               , ctxTarget = target
-               }
-    assocsWithCtx = [ (v, runReaderT mb (ctx v)) | (v, mb) <- assocs ]
-{-# INLINABLE allEvaluators #-}
-
-
+-- | Each variable evaluator has access to this data.
 data Ctx = Ctx
   { ctxVar :: !Var
   , ctxTarget :: !(Path Abs File)
@@ -53,8 +41,47 @@ data Ctx = Ctx
 
 type MonadEvaluator m = (MonadReader Ctx m, MonadError String m, MonadIO m)
 
+data Evaluator m = forall a. EvaluatorResult a => Evaluator !(m a)
+
+class EvaluatorResult a where
+  toByteString :: MonadError String m => a -> m ByteString
+
+instance EvaluatorResult ByteString where
+  toByteString = pure . id
+
+instance EvaluatorResult String where
+  toByteString = pure . B8.pack  -- TODO: use proper UTF-8 encoding
+
+
+rawEvaluators
+  :: forall m. (MonadError String m, MonadIO m)
+  => [(Var, Evaluator (ReaderT Ctx m))]
+rawEvaluators =
+      [ (Var "HASKELLRESOLVER", Evaluator testEvaluator)
+      , (Var "HASKELLMODULE", Evaluator testEvaluator2)
+      ]
+
+allEvaluators
+  :: forall m. (MonadError String m, MonadIO m)
+  => Path Abs File  -- ^ path to target file
+  -> Map Var (m ByteString)
+allEvaluators target = Map.fromList $ applyCtx' <$> rawEvaluators
+  where
+    applyCtx' (v, e) = (v, applyCtx (mkCtx v) e)
+    applyCtx ctx (Evaluator rma) = runReaderT rma ctx >>= toByteString
+    mkCtx v = Ctx{ ctxVar = v
+                 , ctxTarget = target
+                 }
+{-# INLINABLE allEvaluators #-}
+
 testEvaluator :: MonadEvaluator m => m ByteString
 testEvaluator = do
   var <- asks ctxVar
   liftIO $ putStrLn $ "testEvaluator was called for " <> show var
+  pure "HAHAHA"
+
+testEvaluator2 :: MonadEvaluator m => m String
+testEvaluator2 = do
+  var <- asks ctxVar
+  liftIO $ putStrLn $ "testEvaluator2 was called for " <> show var
   pure "HAHAHA"
